1b6ceb9107d27ba599ad46b0d76f3bb369d110f6
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2024-06-11 02:06:09 -0700

    ekejhdk

---
+++ final_proj/main.c
@@ -0,0 +1,18 @@
+/*
+ * final_proj.c
+ *
+ * Created: 6/11/2024 1:49:42 AM
+ * Author : Administrator
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
